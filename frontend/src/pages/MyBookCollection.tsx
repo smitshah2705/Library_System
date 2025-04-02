2ddb38a7deb415745f624992c970bd0cbecc7fb7
Ferdinand1267 <ferdinand.changb2026@stu.tes.tp.edu.tw>
2025-04-03 00:11:58 +0800

    done

--- frontend/src/pages/MyBookCollection.tsx
+++ frontend/src/pages/MyBookCollection.tsx
@@ -1,10 +1,10 @@
 import "./MyBookCollection.css";
 import { useState } from "react";
 function MyBookCollection() {
-    const [borrowed, setBorrowed] = useState<{ id: number; title: string; author: string; dueDate: string }[]>([]);
+    const [borrowed, setBorrowed] = useState<{ id: number; title: string; author: string }[]>([]);
     const username = localStorage.getItem("username") || "";
 
-    const handleBorrow = async () => {
+    const handleBorrowed = async () => {
         try {
             const response = await fetch(`http://localhost:8080/users/${encodeURIComponent(username)}/borrowed-books`);
             const data = await response.json();
@@ -19,10 +19,19 @@ function MyBookCollection() {
         try {
             const response = await fetch(`http://localhost:8080/books/${bookID}/return`, { method: "PUT" });
             alert("Book returned!");
-            handleBorrow();
+            handleBorrowed();
         }
         catch (error) {
             alert("Error occured, unable to return book")
+        }
+    }
+    const isOverDue = async (username: String) => {
+        try{
+            const response = await fetch (`http://localhost:8080/books/checkoverdue/${username}`, {method: "PUT"});
+            console.log(await response.text());
+        }
+        catch (error) {
+            alert("Error checking whether overdue")
         }
     }
     
@@ -30,8 +39,22 @@ function MyBookCollection() {
         <div>
             <img src="/teslogotransparent.png" alt="TES" width="200" className="logo" />
             <h1>My Books:</h1>
-            <button onClick={handleBorrow}>Load Borrowed Books</button>
-
+            <button onClick={handleBorrowed}>Load Borrowed Books</button>
+            <button onClick={() => isOverDue(username)}>Check for Overdue Books</button>
+            <div className="book-list">
+                {borrowed.length > 0 ? (
+                    <ul>
+                        {borrowed.map((book) => (
+                            <li key={book.id}>
+                                {book.title} - {book.author}
+                                <button onClick={() =>handleReturn(book.id)}>Return</button>
+                            </li>
+                        ))}
+                    </ul>
+                ):(
+                    <p>No books currently borrowed</p>
+                )}
+            </div>
         </div>
     );
 }
