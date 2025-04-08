f6e8cf44aab6df97d105b530cff8e52cf7ed4f60
jackhsieh06 <26jackh@stu.tes.tp.edu.tw>
2025-04-08 19:46:10 +0800

    Update db_library.sql

--- db_library.sql
+++ db_library.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS books;
 
--- Create the books table
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -9,13 +8,12 @@ CREATE TABLE books (
     borrowedDate DATE
 );
 
--- Insert 10 books
 INSERT INTO books (title, author, isAvailable, borrowedDate) VALUES
-('1984', 'George Orwell', TRUE, NULL),
-('The Hobbit', 'J.R.R. Tolkien', FALSE, '2025-04-01'),
+('Romeo and Juliet', 'William Shakespear', TRUE, NULL),
+('Harry Potter and the Half Blood Prince', 'J.K Rowling', FALSE, '2025-04-01'),
 ('To Kill a Mockingbird', 'Harper Lee', TRUE, NULL),
 ('Brave New World', 'Aldous Huxley', TRUE, NULL),
-('Fahrenheit 451', 'Ray Bradbury', FALSE, '2025-03-28'),
+('War and Peace', 'Leo Tolstoy', FALSE, '2025-03-28'),
 ('Animal Farm', 'George Orwell', TRUE, NULL),
 ('The Great Gatsby', 'F. Scott Fitzgerald', TRUE, NULL),
 ('Jane Eyre', 'Charlotte Brontë', FALSE, '2025-04-03'),
